702b570856ba0e4e1be8cf5150ffc2b23621ea9c
Abhisheksandeepa <abhisheks@dtechlk.com>
2026-05-14 14:41:56 +0530

    update daily taSK

--- src/main/java/com/dtech/admin/dto/response/DailyTaskMedicalRowDTO.java
+++ src/main/java/com/dtech/admin/dto/response/DailyTaskMedicalRowDTO.java
@@ -1,24 +1,32 @@
 package com.dtech.admin.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 @Data
 public class DailyTaskMedicalRowDTO {
     private String staffType;
     private String date;
+    @JsonIgnore
     private long claimsReceived;
     private String claimsReceivedDetails;
+    @JsonIgnore
     private long notYetProcessed;
     private String notYetProcessedDetails;
     private DailyTaskReportStageDTO firstCheckComplete;
+    @JsonIgnore
     private long pendingRequirementClaims;
     private DailyTaskReportStageDTO haveToPreparePaymentAttachments;
     private DailyTaskReportStageDTO preparePaymentAttachments;
+    @JsonIgnore
     private DailyTaskReportStageDTO haveToHandoverForFinalCheck;
+    @JsonIgnore
     private DailyTaskReportStageDTO handoverForFinalCheck;
     private DailyTaskReportStageDTO haveToCompleteFinalCheck;
     private DailyTaskReportStageDTO finalCheckComplete;
+    @JsonIgnore
     private DailyTaskReportStageDTO haveToInputToCurrentSystem;
+    @JsonIgnore
     private DailyTaskReportStageDTO inputToCurrentSystem;
     private DailyTaskReportStageDTO haveToPaymentsComplete;
     private DailyTaskReportStageDTO paymentsCompleted;
