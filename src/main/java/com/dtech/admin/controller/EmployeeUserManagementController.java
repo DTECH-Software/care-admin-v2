6aba1d9532bdff5fb4d80be5577879618082a7f9
Abhisheksandeepa <abhisheks@dtechlk.com>
2026-07-06 13:16:13 +0530

    Update Employee

--- src/main/java/com/dtech/admin/controller/EmployeeUserManagementController.java
+++ src/main/java/com/dtech/admin/controller/EmployeeUserManagementController.java
@@ -83,4 +83,11 @@ public class EmployeeUserManagementController {
         return employeeUserManagementService.staffCategoryUpdate(gson.fromJson(gson.toJson(employeeManagementRequestValidatorDTO), EmployeeManagementRequestDTO.class), locale);
     }
 
+    @PostMapping(path = "/update",produces = MediaType.APPLICATION_JSON_VALUE,consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Handle employee staff category update request request ",notes = "Employee staff category update request success or failed")
+    public ResponseEntity<ApiResponse<Object>> update(@RequestBody @Validated(OnStaffCategoryUpdate.class) @Valid EmployeeManagementRequestValidatorDTO employeeManagementRequestValidatorDTO, Locale locale) {
+        log.info("Employee user update request controller {} ", employeeManagementRequestValidatorDTO);
+        return employeeUserManagementService.staffCategoryUpdate(gson.fromJson(gson.toJson(employeeManagementRequestValidatorDTO), EmployeeManagementRequestDTO.class), locale);
+    }
+
 }
